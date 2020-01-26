26ffda0847977bfab1498d8193a8a65f3e610135
Jérôme <jvillafruela@users.noreply.github.com>
2020-01-26 23:52:53 +0100

    main : artwork + author

--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
-func main() {
-	/*
-		ref := "09940004427"
-		a, err := joconde.GetArtwork(ref)
-		if err != nil {
-			log.Fatal("Error : ", err)
-		}
-		fmt.Printf("%v \n", a)
-	*/
+import (
+	"log"
 
-	/*
-		js, err := wd.FindAuthor("Georges de La Tour")
-		if err != nil {
-			log.Fatal("Error : ", err)
-		}
-		fmt.Printf("%v \n", js)
-	*/
+	"github.com/JVillafruela/wdjqs/joconde"
+	"github.com/JVillafruela/wdjqs/wd"
+)
+
+func main() {
+
+	ref := "09940004427"
+
+	a, err := joconde.GetArtwork(ref)
+	if err != nil {
+		log.Fatal("Error : ", err)
+	}
+	log.Printf("Found ref %s in joconde database : %s\n", ref, a.Title)
+
+	log.Printf("Looking for author %s\n", joconde.ReverseName(a.Author))
+	qid, err := wd.FindAuthor(joconde.ReverseName(a.Author))
+	if err != nil {
+		log.Fatal("Error : ", err)
+	}
+	if qid != "" {
+		log.Printf("Found author : %s \n", qid)
+	}
 
 	/*
 		qid, err := wd.FindMuseumByMuseoID("M0994")
